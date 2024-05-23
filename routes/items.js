e1f61ec309d05498d73077bc5f1ad5e5c8b89e8f
Alago Victor <123035877+victor-alago@users.noreply.github.com>
2024-05-23 10:46:29 +0200

    created the routes for the crud services

---
+++ routes/items.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const { createItem } = require('../services/createItem');
+const { readItems } = require('../services/readItems');
+const { updateItem } = require('../services/updateItem');
+const { deleteItem } = require('../services/deleteItem');
+
+// Create an item
+router.post('/create/', createItem);
+
+// Read all items
+router.get('/', readItems);
+
+// Update an item
+router.put('update/:id', updateItem);
+
+// Delete an item
+router.delete('delete/:id', deleteItem);
+
+module.exports = router;
